890f815ec4a0d7b30332e252a652bcee9ad868b0
Tessyjames <tessyjames28@gmail.com>
2024-03-31 13:41:02 +0100

    Model

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from .fields import IntegerRangeField
+
 
 # Create your models here.
+class Booking(models.Model):
+    name = models.CharField(max_length = 255)
+    no_of_guests = IntegerRangeField(min_value=1, max_value=6)
+    booking_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.name
+    
+
+class Menu(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = IntegerRangeField(min_value=1, max_value=6)
+
+    def __str__(self):
+        return self.title
