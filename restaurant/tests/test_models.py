f8e902c3f925795beebc6470de50dd523afb8a82
Tessyjames <tessyjames28@gmail.com>
2024-04-01 23:57:46 +0100

    Test files

--- restaurant/tests/test_models.py
+++ restaurant/tests/test_models.py
@@ -1,5 +1,5 @@
 from django.test import TestCase, Client
-from ..models import Menu, Booking
+from ..models import Menu
 from ..serializers import MenuSerializer
 
 # Create your tests here.
@@ -9,30 +9,3 @@ class MenuTest(TestCase):
         itemstr = item.__str__()
 
         self.assertEqual(itemstr, "Fried Rice : 50.00")
-
-class MenuViewTest(TestCase):
-    def setUp(self):
-        Menu.objects.create(title="Fried Rice", price=50.00, inventory=30)
-        Menu.objects.create(title="Jellof Rice", price=40.00, inventory=30)
-        Menu.objects.create(title="Stew Rice", price=45.00, inventory=30)
-        
-        self.client = Client()
-
-    def test_getall(self):
-        # Make a GET request to the view url
-        response = self.client.get('/restaurant/menu/')
-
-        # check response status
-        self.assertEqual(response.status_code, 200)
-
-        # Retrieve all items from the database
-        menu_items = Menu.objects.all()
-
-        # Serialize the menu items
-        serializer = MenuSerializer(menu_items, many=True)
-
-        # Compare serialize data with response
-        self.assertEqual(response.data, serializer.data)
-
-        # Check total number of menu items
-        self.assertEqual(len(response.data), menu_items.count())
